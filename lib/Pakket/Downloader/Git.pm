b2b2a58f4f58fb1996a29fabe9328483f6efec46
Andrew Grechkin <andrew.grechkin@gmail.com>
2018-07-18 14:15:42 +0200

    substitute username and password in git download module

--- lib/Pakket/Downloader/Git.pm
+++ lib/Pakket/Downloader/Git.pm
@@ -8,6 +8,7 @@ use Types::Path::Tiny   qw< Path >;
 use Carp                qw< croak >;
 use Log::Any            qw< $log >;
 use Git::Wrapper;
+use URI::Escape;
 use namespace::autoclean;
 
 extends qw< Pakket::Downloader >;
@@ -23,6 +24,23 @@ sub BUILD {
     ($self->{url}, $self->{commit}) = split('#', $self->url);
     $self->{url} =~ s|^git://||;
     $self->{url} =~ s|^git[+-]||;
+
+    # substitute GIT_PASSWORD and GIT_USERNAME variables in URI
+    if ($self->{url} =~ m/\$GIT_PASSWORD/) {
+        if ($ENV{GIT_PASSWORD}) {
+            my $pass = uri_escape($ENV{GIT_PASSWORD});
+            $self->{url} =~ s/\$GIT_PASSWORD/$pass/g;
+        } else {
+            $self->{url} =~ s/:\$GIT_PASSWORD//g;
+        }
+    }
+    if ($self->{url} =~ m/\$GIT_USERNAME/) {
+        $ENV{GIT_USERNAME} //= $ENV{USER} //= 'git';
+        my $name = uri_escape($ENV{GIT_USERNAME});
+        $self->{url} =~ s/\$GIT_USERNAME/$name/g;
+    }
+
+    return 0;
 }
 
 sub download_to_file {
